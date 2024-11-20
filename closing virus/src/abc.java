a94ba6c7465939c82a40e521a859af77327b04a2
Dev-Pratham <pratham.chaurasia31@gmail.com>
2024-11-20 20:59:36 +0530

    Programs using java ide

---
+++ closing virus/src/abc.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+public class abc{
+    public static void main(String args[]){
+        int x=0;
+        int y=50;
+        while (x<y){
+            JOptionPane.showConfirmDialog(null,"do you want to close this virus?","virus",JOptionPane.YES_NO_OPTION );
+            
+        }
+        
+    }
+
+    
+}
---
+++ nested for loop/src/nested/pkgfor/loop/NestedForLoop.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nested.pkgfor.loop;
+
+/**
+ *
+ * @author window
+ */
+public class NestedForLoop {
+
+    private static int i;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+        {
+          System.out.println("\n");
+          
+        }
+                 for(int j=1; j<=i; j++)
+                 {
+                               System.out.println("*");
+
+                 }
+    }
+    
+}
